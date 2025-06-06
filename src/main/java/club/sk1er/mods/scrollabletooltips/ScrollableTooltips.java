17891fac7a9762db37917dd1c668a3ee5fbac1c4
jadeposting <sanctuary.third@gmail.com>
2025-06-06 11:34:48 -0400

    change: change modid to text_overflow_scroll

--- src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
+++ src/main/java/club/sk1er/mods/scrollabletooltips/ScrollableTooltips.java
@@ -15,7 +15,7 @@ package club.sk1er.mods.scrollabletooltips;
 //$$ @Mod(ScrollableTooltips.MOD_ID)
 //#endif
 public class ScrollableTooltips {
-    public static final String MOD_ID = "scrollable_tooltips";
+    public static final String MOD_ID = "text_overflow_scroll";
     public static final String MOD_VERSION = "1.4.0";
     public static final String MOD_NAME = "Scrollable Tooltips";
 
